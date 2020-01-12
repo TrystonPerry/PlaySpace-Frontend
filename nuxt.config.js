7491bfdaaec36c15741ea73aec4ad8791c60befc
Tryston Perry <Tryston@TrystonPerry.com>
2020-01-12 10:12:21 -0800

    Moved .css file to html instead

--- nuxt.config.js
+++ nuxt.config.js
@@ -47,6 +47,10 @@ module.exports = {
     link: [
       { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
       {
+        rel: "stylesheet",
+        href: "/main.css"
+      },
+      {
         type: "stylesheet",
         href: "https://fonts.googleapis.com/css?family=Source+Sans+Pro"
       }
@@ -68,7 +72,7 @@ module.exports = {
   /*
    ** Global CSS
    */
-  css: ["~/assets/tailwind.css", "~/assets/main.css"],
+  css: ["~/assets/tailwind.css"],
   /*
    ** Plugins to load before mounting the App
    */
