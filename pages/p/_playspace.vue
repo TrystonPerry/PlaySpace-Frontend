d72b154c1da4106df5335bdc1d783460931b8b2d
Tryston Perry <Tryston@TrystonPerry.com>
2020-02-15 08:11:46 -0800

    Removed unneeded sections

--- pages/p/_playspace.vue
+++ pages/p/_playspace.vue
@@ -36,35 +36,7 @@
               <h1 class="text-2xl font-bold">{{ playSpace.channelName }}</h1>
               <h2 class="text-lg">{{ playSpace.title }}</h2>
             </div>
-            <div class="md:w-6/12 md:text-right">
-              <h2
-                class="text-xl mt-4 font-bold inline-block border-b border-gray-300 px-6 md:pr-0 mb-2"
-              >
-                Users
-              </h2>
-              <ul class="list-style-none">
-                <li v-for="user in users" :key="user.username">
-                  <span>{{ user.rank }} - </span>
-                  <h3 class="font-bold inline-block">
-                    {{ user.username }}
-                  </h3>
-                </li>
-              </ul>
-            </div>
           </div>
-          <!-- <div
-            class="absolute flex"
-            style="left:50%;bottom:-1.5rem;transform:translateX(-50%);"
-          >
-            <p-btn variant="primary" size="lg" class="mr-1">
-              <p-icon icon="fas fa-plus" />
-              Start a Stream
-            </p-btn>
-            <p-btn variant="none" size="lg" class="bg-dark-2">
-              <p-icon icon="fas fa-cog" />
-              Settings
-            </p-btn>
-          </div> -->
           <p-avatar
             :avatar="playSpace.avatar"
             class="w-full h-full absolute top-0 left-0 object-cover bg-gray-100 opacity-25"
@@ -73,19 +45,6 @@
             style="z-index:-1; filter: blur(4px)"
           />
         </div>
-
-        <client-only>
-          <div
-            v-if="!totalStreams && canStream && !$store.state.nav.isMobile"
-            class="py-4 mt-6"
-          >
-            <h2 class="text-lg font-bold">
-              Click below to share your desktop, a YouTube video, or Twitch
-              Stream
-            </h2>
-            <AddStream class="max-w-48 w-full mx-auto mt-5" />
-          </div>
-        </client-only>
       </div>
 
       <PlaySpaceMobileSidebar
@@ -124,11 +83,11 @@
       <portal to="modal-container">
         <div
           v-if="$store.state.stream.isSoundBlocked"
-          class="absolute w-full p-2 max-w-96 w-full"
+          class="absolute w-full max-w-96 w-full"
           style="bottom: 4rem;left:50%;transform:translateX(-50%);z-index:100"
         >
           <div
-            class="bg-black-600 text-black-800 shadow-reg py-3 px-5 rounded-lg border-4 border-black-800"
+            class="bg-dark-5 text-gray-300 text-center shadow-reg py-3 px-5 rounded-lg shadow-xl"
           >
             <h1 class="text-xl font-bold text-center">
               Sound Muted by Default
@@ -156,7 +115,7 @@
 
 <script>
 import { Device } from "mediasoup-client"
-import { mapGetters, mapActions } from "vuex"
+import { mapState, mapGetters, mapActions } from "vuex"
 
 import Vue from "vue"
 import API from "@/api/api"
@@ -223,6 +182,10 @@ export default {
   },
 
   computed: {
+    ...mapState({
+      username: state => state.user.username
+    }),
+
     ...mapGetters({
       totalStreams: "stream/totalStreams",
       users: "playSpace/users"
@@ -238,19 +201,19 @@ export default {
   sockets: {
     API: {
       "room-users-update"(user) {
-        if (
-          user.username === this.$store.state.user.username &&
-          user.rank !== "none"
-        ) {
+        const { username, rank } = user
+
+        // If you were granted access to playspace
+        if (username === this.username && rank !== "none") {
           this.$notify({
             type: "success",
             title: "You have been granted access to this PlaySpace",
             text: `You can now stream on ${this.playSpace.channelName}`
           })
-        } else if (
-          user.username === this.$store.state.user.username &&
-          user.rank === "none"
-        ) {
+        }
+
+        // If you were revoked access to this playspace
+        else if (username === this.username && rank === "none") {
           // TODO handle better
           location.reload()
           this.$notify({
@@ -259,6 +222,7 @@ export default {
             text: `You can no longer stream on ${this.playSpace.channelName}`
           })
         }
+
         this.$store.dispatch("playSpace/updateUserRankInCurrentPlaySpace", user)
       }
     },
